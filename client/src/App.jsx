dcdf4d546ecaee9221c0f6d0316aa077e829c73d
Antonin Tacchi <antonin.tacchi@laplateforme.io>
2025-07-08 14:04:43 +0200

    Fix : update import path for AppRouter component in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { AppRouter } from "../routes/AppRouter"
+import { AppRouter } from "./routes/AppRouter"
 
 export function App() {
   return <AppRouter />
